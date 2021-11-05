55e028de09513e17b978fa05ff8c18c190347562
Mir Misbah Ali <mirmisbahali@hotmail.com>
2021-11-05 16:08:28 +0530

    created home page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,10 +9,3 @@ for (let i = 0; i < navLinks.length; i++) {
   }
 }
 
-const projectId = "y0zewcru";
-const query = `*[_type == "post"]{title, slug, body}`;
-const url = `https://${projectId}.api.sanity.io/v2021-06-07/data/query/production?query=${query}`;
-
-fetch(url).then((res) => {
-  res.json().then((data) => console.log(data.result[0]));
-});
--- src/index.md
+++
@@ -1,8 +0,0 @@
----
-title: Home Page
-layout: base.njk
----
-
-<h1 class="display-1">
-    <a href="/blog" class="text-dark">View Blog</a>
-</h1>
